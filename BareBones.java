eeda62c834642177f8b60cb5324e7961d9652718
Xrischo <72524242+Xrischo@users.noreply.github.com>
2020-10-25 03:29:04 +0000

    Update BareBones.java

--- BareBones.java
+++ BareBones.java
@@ -18,7 +18,7 @@ public class BareBones {
 		System.out.println("Choose a name for your code: ");
 		
 		Scanner input = new Scanner(System.in);
-		String fileName = "src/" + input.nextLine() + ".txt";
+		String fileName = "src/" + input.nextLine() + ".txt"; // concatenate file directiory
 		
 		System.out.println("-------------------------");
 		System.out.println(fileName + " {");
@@ -28,9 +28,9 @@ public class BareBones {
 	}
 	
 	private void humanInput(String fileName, Scanner input) {
-		// put all acceptable codelines in arraylist
+		// put all acceptable codelines in arraylist split from string
 		String instrLine = "clear a; incr a; decr a; while a not 0 do; end;";
-		String[] instrArr = instrLine.split("(?<=;)\\s");
+		String[] instrArr = instrLine.split("(?<=;)\\s"); // positive look behind, if ; is followed by whitespace
 		List<String> instrList = new ArrayList<String>();
 		instrList = Arrays.asList(instrArr);
 		
@@ -42,7 +42,7 @@ public class BareBones {
 			
 			while (blockLevel > 0) {
 				codeLine = input.nextLine();
-				int indentC = 0;
+				int indentC = 0; // indentation level
 				while (Character.isWhitespace(codeLine.charAt(indentC))) {
 					indentC++;
 				}
@@ -87,12 +87,12 @@ public class BareBones {
 
 		//putting the whole barebones program in a while loop and going line by line
 		while (count+1 <= codeBB.size() && (codeLine = codeBB.get(count++)) != null) {
-			instruction = getInstr(codeLine);
-			var = getVar(codeLine);
+			instruction = getInstr(codeLine); // gets the instruction from the line
+			var = getVar(codeLine); // gets the variable name
 			
 			if (instruction.equals("end")) { // base case for recursion
 				return;
-			} else if (!variables.containsKey(var)) {
+			} else if (!variables.containsKey(var)) { // if it's not "end" then there is some variable
 				variables.put(var, 0);
 			}
 			value = variables.get(var);
@@ -106,34 +106,34 @@ public class BareBones {
 				variables.put(var, value-1);
 			} else if (instruction.equals("while")) { // making the while loop as recursion that ends at "end", then checks again
 				while (value != 0) {
-					if (loopVar.size() == 0) {
+					if (loopVar.size() == 0) { // if it's the first iteration of the loop, store the value of the variable
 						loopVar.add(value);
-					} else if (loopVar.get(0) - value <= 0) {
+					} else if (loopVar.get(0) - value <= 0) { // if not, check if loop is infinite
 						System.out.println("Infinite loop, faulty code.");
 						System.exit(0);
 					}
-					interpret(count);
+					interpret(count); // recursion
 				}
 					count = loopJump(count); // when value == 0 we need to jump to the end
 			} else {
-				System.out.println("Unknown instruction.");
+				System.out.println("Unknown instruction."); // if somehow an unknown instruction slips in but regex game strong lol
 			}
 		}
 	}
 	
 	// jumping to the end of the loop when var == 0
 	private int loopJump(int count) { 
-		int equalise = 1; // Number of "while" minus number of "end"
+		int blockLevel = 1; // Number of "while" minus number of "end"
 		String check;
 
-		while (equalise != 0) {
+		while (blockLevel != 0) {
 			count++;
 			check = codeBB.get(count);
 			
 			if (getInstr(check).equals("end")) {
-				equalise--;
+				blockLevel--;
 			} else if (getInstr(check).equals("while")) {
-				equalise++;
+				blockLevel++;
 			}
 		}
 		
@@ -153,16 +153,19 @@ public class BareBones {
 		}
 	}
 	
+	//get first word (instruction)
 	private String getInstr(String codeLine) {
 		return codeLine.replaceAll("^\\s*|[^\\w*].*", "");
 	}
 	
+	//get second word (variable)
 	private String getVar(String codeLine) {
 		return codeLine.replaceAll(("^\\s*\\w*\\s|;|\\snot.*"), "");
 	}
 	
+	//at the end print every variable and its value
 	private void printVar() {
-		 variables.forEach((name, value) -> System.out.println(name + " = " + value));
+		 variables.forEach((name, value) -> System.out.println(name + " = " + value)); 
 	}
 	
 	public static void main(String[] args) {
